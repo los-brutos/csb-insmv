184a0e7a43d67035e049a2dc843219b53cac21cd
Los Brutos <117457669+los-brutos@users.noreply.github.com>
2026-02-13 18:06:59 +0100

    INSMV : fiche de personnage + fiche de pouvoir
    avec le module et les pack/unpack (.bat)

---
+++ pack.bat
@@ -0,0 +1,28 @@
+@echo off
+REM 
+REM création des compendiums du module CSB Basic Instinct System
+REM
+REM le paquet fvtt doit être  installé https://github.com/foundryvtt/foundryvtt-cli
+REM
+REM /!\ nécessite que l'instance de foundry soit fermée avant de lancer la commande
+REM
+@echo off
+cls
+echo * ********************************* *
+echo * start create compendiums          *
+echo * ********************************* *
+echo.
+echo /// configure environment
+call fvtt package workon csb-insmv --type=Module
+echo.
+echo /// pack compendium "actor-templates"
+call fvtt package pack actor-templates
+echo.
+echo /// pack compendium "item-templates"
+call fvtt package pack item-templates
+echo.
+echo * ********************************* *
+echo * all compendiums have been created *
+echo * ********************************* *
+echo.
+@echo on
---
+++ unpack.bat
@@ -0,0 +1,28 @@
+@echo off
+REM 
+REM dépaquetage des compendiums du module Bitume Mk5
+REM
+REM le paquet fvtt doit être  installé https://github.com/foundryvtt/foundryvtt-cli
+REM
+REM /!\ nécessite que l'instance de foundry soit fermée avant de lancer la commande
+REM
+@echo off
+cls
+echo * ********************************** *
+echo * start unpack compendiums           *
+echo * ********************************** *
+echo.
+echo /// configure environment
+call fvtt package workon csb-insmv --type=Module
+echo.
+echo /// unpack compendium "actor-templates"
+call fvtt package unpack -c actor-templates
+echo.
+echo /// unpack compendium "item-templates"
+call fvtt package unpack -c item-templates
+echo.
+echo * ********************************** *
+echo * all compendiums have been unpacked *
+echo * ********************************** *
+echo.
+@echo on
